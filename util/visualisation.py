60682091a4f88ff5cabffbc7a7b5e4542822127c
JulienRouse <julien.rouse@gmail.com>
2015-01-26 23:45:18 +0100

    waaa on peut voir les courbes!

---
+++ util/visualisation.py
@@ -0,0 +1,42 @@
+'''
+A small lib that will hopefully help me plot some of my tp
+and help me visualize whats going on in here.
+
+'''
+
+import pylab
+import numpy as np
+
+    
+
+def plot_points_cloud(x,y):
+    '''
+    Plot points cloud.
+    
+    :param x: a vector containing the x-coordinates for the points to plot
+    :param y: a vector containing the y-coordinates for the points to plot
+    :type x: list of float
+    :type y: list of float
+    :return: nothing
+    :rtype: void
+    :side-effect: Create a png resulting from the plotting, and a popup
+                  showing the plot.
+    '''
+    pylab.plot(x,y,'b.')
+
+
+
+def plot_tp3(theta,n=20):
+    '''
+    Plot a line
+    '''
+    size = theta[n].shape[1]
+    x = np.arange(-2.5,2.6,0.1)
+    y = np.zeros(x.shape)
+    for i in xrange(0,size):
+        tmp = theta[n][0,i]*x**i
+        y = y + tmp
+    pylab.plot(x,y,'g-')
+
+if __name__ == '__main__':
+    pass
